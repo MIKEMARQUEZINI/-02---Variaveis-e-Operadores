747e2368438807300f497c2fbca60a61dd7a8eb8
MIKE <marquezini.mike@outlook.com>
2023-04-13 12:36:09 -0300

    versao 2

--- Exerc4.java
+++ Exerc4.java
@@ -10,18 +10,18 @@ public class Exerc4 {
 		
 		Scanner leia = new Scanner (System.in);
 		
-		System.out.println("Entre com a sua 1o Número: ");
+		System.out.println("Entre com a seu 1o Número: ");
 		num1 = leia.nextFloat();		
-		System.out.println("Entre com a sua 2o Número: ");
+		System.out.println("Entre com a seu 2o Número: ");
 		num2 = leia.nextFloat();
-		System.out.println("Entre com a sua 3o Número: ");
+		System.out.println("Entre com a seu 3o Número: ");
 		num3 = leia.nextFloat();		
-		System.out.println("Entre com a sua 4o Número: ");
+		System.out.println("Entre com a seu 4o Número: ");
 		num4 = leia.nextFloat();
 		
 		x = (num1*num2) - (num3*num4);
 		
-		System.out.printf("\nA diferença entre os números digitados é : %.1f",x);
+		System.out.printf("\nA diferença entre os números digitados é de %.1f",x);
 
 	}
 
